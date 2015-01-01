d083516f648c30c448f8f4037e5435b9aef1a4c3
roystharayil <roystharayil@gmail.com>
2015-01-01 20:47:14 +0530

    Server and Application created

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <Wt/WApplication>
+#include <Wt/WContainerWidget>
+
+using namespace Wt;
+
+WApplication* createApplication(const WEnvironment& env)
+{
+    WApplication *app = new webpage(env);
+
+    return app;
+}
+
+int main(int argc, char** argv)
+{
+
+    return WRun(argc, argv, &createApplication);
+}
 
