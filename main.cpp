ac9bf4d6389d5bd9f48fb4727907ad105d4901dc
roystharayil <roystharayil@gmail.com>
2015-01-01 22:02:48 +0530

    Add webpage Application class

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@
 
 using namespace Wt;
 
+/*
+ * A simple web page application class.
+ */
+class webpage : public Wt::WApplication
+{
+public:
+  webpage(const WEnvironment& env);
+
+};
+
+//---------create webpage object---------------------------------
+webpage::webpage(const WEnvironment &env) : Wt::WApplication(env)
+{
+    setTitle("simple!!!");
+}
+
+//--------------------------------------------------------------
 WApplication* createApplication(const WEnvironment& env)
 {
     WApplication *app = new webpage(env);
-
     return app;
 }
 
@@ -15,4 +31,4 @@ int main(int argc, char** argv)
 
     return WRun(argc, argv, &createApplication);
 }
-
+//--------------------------------------------------------------
